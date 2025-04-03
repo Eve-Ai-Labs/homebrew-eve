54e250b820a654fd6d4d5e0b986b749c884acbb8
eveaidev <dev@eveai.dev>
2025-04-03 12:13:48 -0400

    Update sha256 for binaries

--- Formula/eve.rb
+++ Formula/eve.rb
@@ -6,16 +6,16 @@ class Eve < Formula
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/Eve-Ai-Labs/eve/releases/download/eve-v0.1.0/eve-cli-v0.1.0-macos-arm64.zip"
-        sha256 "ed8c282a2a548b2f956acde82e010270378cfa10b77044563864b91ebb0bf49b"
+        sha256 "3a92e4914b59199897902897d3741767aad93ac0b0762232780e5d776d51ed62"
       else
         url "https://github.com/Eve-Ai-Labs/eve/releases/download/eve-v0.1.0/eve-cli-v0.1.0-macos-x86_64.zip"
-        sha256 "9360b54e9e447c4fd99636f233928b9d1ef93524c4f1742d3a76521cd2e2d9e1"
+        sha256 "ff9ba81bced5bdec48c64c94a6fd8b9226bd9f82e2a531d1431ec79a566e97c3"
       end
     end
   
     on_linux do
         url "https://github.com/Eve-Ai-Labs/eve/releases/download/eve-v0.1.0/eve-cli-v0.1.0-linux-x86_64.zip"
-        sha256 "731f54a7fb6c944a920f8d4e021131c3914ea5045103d3efea9b0c6e03118030"
+        sha256 "cb76214f42a8001dc46271c6b939edbf382cade0a182896e075dc1745e79af00"
       end
   
     def install
